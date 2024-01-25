fd09a522aa12186a09ed06f1530da1237fdf0126
ernilson <ernilson25@gmail.com>
2024-01-25 20:45:05 -0300

    refatorado o swagger

--- src/main/java/br/com/devsibre/config/SwaggerConfig.java
+++ src/main/java/br/com/devsibre/config/SwaggerConfig.java
@@ -14,10 +14,11 @@ public class SwaggerConfig {
 	public OpenAPI Devsibre() {
 		return new OpenAPI().info(new Info()
 				.title("Devsibre")
-				.description("Portal de cadastros")
+				.description("Segunda Igreja Batista no Recanto das Emas")
 				.version("1.0.0")
 				.license(new License().name("Sistema de Cadastro - Portal Sibre").url("https://site-sibre.netlify.app/"))
 		);
 	}
 	// --> http://localhost:8080/swagger-ui/index.html
+	// http://localhost:8080/swagger-ui.html
 }
